ecbbfdd279f6fa1db5dd56ff2c9e0b97aa1b709b
Sergiopavonalvarez <118288663+Sergiopavonalvarez@users.noreply.github.com>
2023-01-02 14:54:53 +0100

    CURSO_JAVA

--- src/poo_2/coche.java
+++ src/poo_2/coche.java
@@ -60,4 +60,20 @@ public class coche {
 		this.motor = motor;
 	}
 
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		coche Renault = new coche();
+
+		System.out.println("Este coche tiene " + Renault.getRuedas() + " ruedas");
+
+		System.out.println("este coche pesa " + Renault.getPeso());
+
+		System.out.println("Tiene de largo: " + Renault.getLargo() + " cm");
+		
+		System.out.println(Renault.getMotor() + " de mmotor");
+
+	}
+	
 }
